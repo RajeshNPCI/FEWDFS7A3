783b18cbe6e9b8389aa63ee1740a1c4ff92b8f01
RajeshNPCI <p.rajesh531@gmail.com>
2017-01-31 00:55:17 +0530

    FEWDFS7A3
    FEWDFS7A3

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function counter(){			
+	var count = 0;				
+	return function(){
+	return ++count;
+	};		
+};			
+var incrementCounter = counter();			
+function onButtonClick(){			
+	var count = incrementCounter();				
+	console.log(count);
+}
